3f7439a9fc021a832d905239010c58eb8fe8d5e9
alakumk <alakumk@IT004>
2020-10-07 17:47:39 +0200

    Configure Identity

--- API/Startup.cs
+++ API/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Hosting;
 using Persistence;
 using FluentValidation.AspNetCore;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace API
 {
@@ -46,6 +48,11 @@ namespace API
                 {
                     cfg.RegisterValidatorsFromAssemblyContaining<Create>();
                 });
+
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext>();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,10 +1,11 @@
 ﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
@@ -14,6 +15,8 @@ namespace Persistence
         public DbSet<Activity> Activities { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+
             //builder.Entity<Activity>()
             //    .HasData(
             //        new Activity { Id = 1, Name = "Value 101"},
